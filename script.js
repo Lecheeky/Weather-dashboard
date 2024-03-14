e622e98b757e840e26bc955a8fed5719652b1dbd
Lecheeky <thadjlakehal@gmail.com>
2024-03-14 14:51:53 +0000

    adding event listener to search fior data when user clicks search btn

--- script.js
+++ script.js
@@ -2,3 +2,8 @@ const cityInput = document.querySelector(".form-input")
 const searchButton = document.querySelector(".search-button")
 const forecastContainer = document.querySelector("#forecast")
 const history = document.querySelector("#history")
+
+searchButton.addEventListener("click", function(event){
+    event.preventDefault()
+    presentCityWeather()
+})
