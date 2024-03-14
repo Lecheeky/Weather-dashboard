cf45b4f9e900309d611621f289a3c95a6bd55b24
Lecheeky <thadjlakehal@gmail.com>
2024-03-14 14:50:21 +0000

    targeting html elements globally using querySelector

---
+++ script.js
@@ -0,0 +1,4 @@
+const cityInput = document.querySelector(".form-input")
+const searchButton = document.querySelector(".search-button")
+const forecastContainer = document.querySelector("#forecast")
+const history = document.querySelector("#history")
